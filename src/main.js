547824941d97e6694d2b9414467a229150efd9e3
Dusan Cajic <dusancajic89@gmail.com>
2023-11-27 17:59:50 +0100

    sticky header inegrated.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
 import "./index.css";
 import { Carousel } from "flowbite";
 import { throttle } from "lodash";
+import $ from "jquery";
 
 const runOnScroll = () => {
-  console.log("function fired!");
-
   if (
     document.body.scrollTop > 132 ||
     document.documentElement.scrollTop > 132
   ) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
+  }
+
+  if (
+    document.body.scrollTop > header + sectionCarousel - 1 ||
+    document.documentElement.scrollTop > header + sectionCarousel - 1
+  ) {
+    headerFixed.style.transform = "translate3d(0, 0, 0)";
+  } else {
+    headerFixed.style.transform = "translate3d(0, -100%, 0)";
   }
 };
 
@@ -20,8 +28,22 @@ const topFunction = () => {
   document.documentElement.scrollTop = 0;
 };
 
-let mybutton = document.getElementById("myBtn");
+const myButton = document.getElementById("btn-top");
 
 window.addEventListener("scroll", throttle(runOnScroll, 250));
+myButton.addEventListener("click", topFunction);
 
-mybutton.addEventListener("click", topFunction);
+let headerFixed = document.querySelector(".headerFixed");
+let header = $(".header").length ? $(".header").outerHeight(true) : 0;
+let sectionCarousel = $("#section-carousel").length
+  ? $("#section-carousel").outerHeight(true)
+  : 0;
+
+const mediaQuery = window.matchMedia("(max-width: 767px)");
+if (mediaQuery.matches) {
+  // If the viewport-width is < 768px terminate the function
+  //return;
+} else {
+  // If the viewport-width is >= 768px set .dropdown-menu top property
+  //return;
+}
